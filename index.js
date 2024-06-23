e8428f328ff8ce247e631a0b3532a26ff7c6008f
Rakeshappu <appussrh@gmail.com>
2024-06-23 08:58:41 +0530

    Calculator

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const input = document.querySelector('.input');
 const result = document.querySelector('.result');
-
+const clear = document.querySelector('.clear');
+const equals = document.querySelector('.equals');
+const calculator = document.querySelector('.calculator');
 
 function handleClickButton(el){
-    console.log(el.innerHTML);
-    const exp = input.value + el.innerHTML;
-    input.value = exp;
-    result.value = eval(exp);
+    if(el != clear && el != equals){
+        const exp = input.value + el.innerHTML;
+        input.value = exp;
+        result.value = eval(exp);
+    }
+    if(el == clear){
+        input.value = '';
+        result.value = '';
+    }
+    
 }
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+const exp = input.value + el.innerHTML;
+    input.value = exp;
+    result.value = eval(exp);
